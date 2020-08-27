0096165329e277cbf27bda67ef34bb6d3b6234f4
Vitaliy Derda <derdavitaliy@gmail.com>
2020-08-27 14:49:21 +0300

    added user session

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const {
 	allowInsecurePrototypeAccess,
 } = require('@handlebars/allow-prototype-access');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
 const addRoutes = require('./routes/add');
@@ -13,32 +15,37 @@ const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user/user');
+const varMiddleware = require('./middleware/variables');
 
 const app = express();
-
+const MONGODB_URI = `mongodb+srv://vitaliy:38vjnQNInjRjmH87@productscluster.dtfx1.mongodb.net/shop`;
 const hbs = exphbs.create({
 	defaultLayout: 'main',
 	extname: 'hbs',
 	handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
 
+const store = new MongoStore({
+	collection: 'sessions',
+	uri: MONGODB_URI,
+});
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(async (req, res, next) => {
-	try {
-		const user = await User.findById('5f4558759498cc42309d1bc8');
-
-		req.user = user;
-		next();
-	} catch (error) {
-		console.log(error);
-	}
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: 'some secret value',
+		resave: false,
+		saveUninitialized: false,
+		store,
+	}),
+);
+
+app.use(varMiddleware);
 
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
@@ -50,27 +57,12 @@ app.use('/auth', authRoutes);
 const PORT = process.env.PORT || 3000;
 
 async function start() {
-	// const someParams = '?retryWrites=true&w=majority';
 	try {
-		const url = `mongodb+srv://vitaliy:38vjnQNInjRjmH87@productscluster.dtfx1.mongodb.net/shop`;
-
-		await mongoose.connect(url, {
+		await mongoose.connect(MONGODB_URI, {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
 			useFindAndModify: false,
 		});
-
-		const candidate = await User.findOne();
-
-		if (!candidate) {
-			const user = new User({
-				email: 'derda.vs@gmail.com',
-				name: 'Vitaliy',
-				cart: { items: [] },
-			});
-
-			await user.save();
-		}
 
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,31 @@
 const { Router } = require('express');
+const User = require('../models/user/user');
 const router = Router();
 
 router.get('/login', async (req, res) => {
 	res.render('auth/login', {
 		title: 'Авторизация',
 		isLogin: true,
+	});
+});
+
+router.get('/logout', async (req, res) => {
+	req.session.destroy(() => {
+		res.redirect('/auth/login#login');
+	});
+});
+
+router.post('/login', async (req, res) => {
+	const user = await User.findById('5f4558759498cc42309d1bc8');
+
+	req.session.user = user;
+	req.session.isAuthenticated = true;
+	req.session.save(err => {
+		if (err) {
+			throw err;
+		}
+
+		res.redirect('/');
 	});
 });
 
